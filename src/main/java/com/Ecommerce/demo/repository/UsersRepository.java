2da58ee1468aea529384efbe4fc7d9a0672c408a
Tsai <965302@gmail.com>
2025-07-09 16:45:18 +0800

    新增 兩個需要的查詢

--- src/main/java/com/Ecommerce/demo/repository/UsersRepository.java
+++ src/main/java/com/Ecommerce/demo/repository/UsersRepository.java
@@ -3,5 +3,11 @@ package com.Ecommerce.demo.repository;
 import com.Ecommerce.demo.model.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<UsersEntity,Integer> {
+
+    Optional<UsersEntity> findByUsername(String username);
+
+    Optional<UsersEntity> findByEmail(String username);
 }
