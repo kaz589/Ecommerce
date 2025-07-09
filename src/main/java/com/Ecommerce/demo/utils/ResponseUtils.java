d8c1ebe73b39f11944967fee434999198818bc90
Tsai <965302@gmail.com>
2025-07-09 16:53:41 +0800

    新增 ResponseUtils 工具類

---
+++ src/main/java/com/Ecommerce/demo/utils/ResponseUtils.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.demo.utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ResponseUtils {
+    public static void sendErrorResponse(HttpServletResponse response, int status, String message) throws IOException {
+        response.setStatus(status);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"" + message + "\"}");
+    }
+}
